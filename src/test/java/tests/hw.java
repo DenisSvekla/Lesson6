e0d95f885a006f6233f60cb8e8000da8933d1bf5
denissv <denis.svekla94@gmail.com>
2021-12-06 21:34:44 +0300

    Произвольная

--- src/test/java/tests/hw.java
+++ src/test/java/tests/hw.java
@@ -1,10 +1,17 @@
 package tests;
 
+import org.testng.annotations.Test;
+
 public class hw {
     public void test() {
 
     }
     public void test1() {
+
+    }
+
+    @Test
+    public void homework_test1() {
 
     }
 }
