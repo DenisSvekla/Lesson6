b2e07d331a0e140de029b9b200a06afee44ef6fd
denissv <denis.svekla94@gmail.com>
2021-12-15 20:45:25 +0300

    xpath New HW

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,14 +1,43 @@
 package pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LoginPage {
     // описание элементов страницы
-    private static By
+    private By PAGE_OPENED_IDENTIFIER = By.className("logo-loginpage");
+
+    private By EMAIL_INPUT = By.id("name");
+
+    private By PASSWORD_INPUT = By.id("password");
+
+    private By LOGIN_BUTTON = By.id("button_primary");
+
+    private WebDriver driver;
     //конструктор страницы
 
 
+    public LoginPage(WebDriver driver) {
+      this.driver = driver;
+
+    }
+
+
+
     //реализация геттеров элементов
+
+    public WebElement getEmailField() {
+        return driver.findElement(EMAIL_INPUT);
+    }
+
+    public WebElement getPasswordField() {
+        return driver.findElement(PASSWORD_INPUT);
+    }
+
+    public WebElement getLoginButton() {
+        return driver.findElement(LOGIN_BUTTON);
+    }
 
     //реализация базовых методов
 
