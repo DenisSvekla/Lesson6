1b6ba0c8c1fcf55c691849dab7d7b0ed97f4c348
denissv <denis.svekla94@gmail.com>
2022-01-17 19:20:13 +0300

    value correct

--- src/test/java/models/Project.java
+++ src/test/java/models/Project.java
@@ -1,22 +1,8 @@
 package models;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 public class Project {
-    private Long id;
-    private String name;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
