ce3e4aafc04b345ab3cda0fc4fe01986c7be0e3c
denissv <denis.svekla94@gmail.com>
2022-02-08 17:34:34 +0300

    111

---
+++ src/test/java/tests/loggerTests.java
@@ -0,0 +1,36 @@
+package tests;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Test;
+
+
+public class loggerTests {
+    private static final Logger logger = LogManager.getLogger(loggerTests.class);
+
+
+    @Test
+    public void logTest() {
+        logger.trace("Trace Message: начало теста....");
+        logger.debug("Debug Message: ....");
+        logger.info("Info Message: ....");
+        logger.error("Error Message: ....");
+        logger.fatal("Fatal Message: ....");
+    }
+
+    @Test
+    public void loginTest() {
+        logger.trace("login test is started...");
+        logger.trace("инициализируем начальные переменные ....");
+        int a = 10;
+        int b = 0;
+
+        if (b==0) {
+            logger.fatal("делитель равен нулю");
+        }
+        int c = a/b;
+
+        logger.trace("login test is finished");
+        }
+    }
+
