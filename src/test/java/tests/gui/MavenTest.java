c77c93204180c32b25f62a2a7c2cbdc48c847c95
denissv <denis.svekla94@gmail.com>
2022-02-16 20:13:36 +0300

    new branch ci_cd

---
+++ src/test/java/tests/gui/MavenTest.java
@@ -0,0 +1,11 @@
+package tests.gui;
+
+import org.testng.annotations.Test;
+
+public class MavenTest {
+
+    @Test
+    public void parametrTest() {
+        System.out.println(System.getProperty("testProp"));
+    }
+}
