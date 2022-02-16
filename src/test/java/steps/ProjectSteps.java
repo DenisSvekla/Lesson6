7c22e5f7925d7ddf639ac51c0d70159470c2ee79
denissv <denis.svekla94@gmail.com>
2022-02-16 21:25:56 +0300

    new branch ci_cd

--- src/test/java/steps/ProjectSteps.java
+++ src/test/java/steps/ProjectSteps.java
@@ -3,10 +3,10 @@ package steps;
 import baseEntity.BaseStep;
 import models.Project;
 import org.openqa.selenium.WebDriver;
-import pages.AddProjectPage;
+
 
 public class ProjectSteps extends BaseStep {
-    private AddProjectPage addProjectPage;
+
 
     //asdsad
     public ProjectSteps(WebDriver driver) {
